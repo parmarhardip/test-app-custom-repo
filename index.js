54a1aaefaaff96d4e9add3f5a605879f690e76c5
GeeksEra <geeksera.online@gmail.com>
2023-11-28 18:47:33 +0400

    custom SignupScreen

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+import { Text, TextInput, View } from "react-native";
+
 export const applyCustomCode = (externalCodeSetup: any) => {
 	// call custom code api here
+	externalCodeSetup.navigationApi.replaceScreenComponent("SignupScreen", () => (
+		<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+			<Text>Hello from custom SignupScreen</Text>
+			<TextInput placeholder={"login"} />
+			<TextInput placeholder={"password"} />
+		</View>
+	));
 };
